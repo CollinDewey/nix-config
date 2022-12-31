ddbf4de3d7eb0c14e9cb8f667e43d0de63b458b2
LegitMagic <76862862+LegitMagic@users.noreply.github.com>
2022-12-31 00:15:22 -0500

    Darwin are you okay?

--- users/collin/home.nix
+++ users/collin/home.nix
@@ -1,7 +1,7 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   home.username = "collin";
-  home.homeDirectory = if pkgs.stdenv.isDarwin then "/Users/collin" else "/home/collin";
+  home.homeDirectory = if pkgs.stdenv.isDarwin then lib.mkForce "/Users/collin" else "/home/collin";
 
   programs.git = {
     userName = "LegitMagic";
