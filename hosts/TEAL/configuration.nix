539e00591049a1282034c87484aaab62b78b54ba
LegitMagic <76862862+LegitMagic@users.noreply.github.com>
2023-01-06 01:27:46 -0500

    Make rclone a system package for TEAL for Duplicati

--- hosts/TEAL/configuration.nix
+++ hosts/TEAL/configuration.nix
@@ -1,9 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services.duplicati = {
     enable = true;
     user = "root"; # yuck
   };
+
+  environment.systemPackages = with pkgs; [
+    rclone # Needed for Duplicati
+  ];
 
   services.netdata.enable = true;
 }
