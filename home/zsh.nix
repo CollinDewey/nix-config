f02cb6c1ea264e3eef059ee805291b7ed55380b2
LegitMagic <76862862+LegitMagic@users.noreply.github.com>
2023-01-10 11:27:33 -0500

    New ZSH theme and different history size in HM module

--- home/zsh.nix
+++ home/zsh.nix
@@ -21,7 +21,21 @@ in {
             sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
           };
         }
+        {
+          name = "agnoster-nix";
+          file = "agnoster-nix.zsh-theme";
+          src = pkgs.fetchgit {
+            url = "https://gist.github.com/chisui/0d12bd51a5fd8e6bb52e6e6a43d31d5e";
+            rev = "a97b74ce17c5f1befabe266ccf02a972cab2911b";
+            sha256 = "sha256-0IFeRc56HvQVNe37+qGvzK+nUrRkf4i/tYLef3NS/7M=";
+          };
+        }
       ];
+      history = {
+        extended = true; # Timestamps
+        size = 1000000; # History loaded in RAM
+        save = 1000000; # History saved to file
+      };
     };
   };
 }
