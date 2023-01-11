3a4d2b5a377a1aa6ff23669c0e3732fa50223d3b
LegitMagic <76862862+LegitMagic@users.noreply.github.com>
2023-01-11 01:39:29 -0500

    Make homebrew more Nix-y and replace terminals on Darwin

--- hosts/COPPER/configuration.nix
+++ hosts/COPPER/configuration.nix
@@ -9,12 +9,14 @@
 
   # TEMPORARY
   homebrew.enable = true;
+  homebrew.onActivation.autoUpdate = true;
+  homebrew.onActivation.cleanup = "zap";
   homebrew.brews = [ "tiger-vnc" ];
   homebrew.casks = [ 
     "firefox"
     "google-chrome"
     "obsidian"
-    "alacritty"
+    "iterm2"
     "barrier"
     "discord"
     "bluebubbles"
@@ -23,6 +25,11 @@
     "alt-tab"
     "appgrid"
   ];
+  homebrew.masApps = {
+    Xcode = 497799835;
+    Testflight = 899247664;
+    Wireguard = 1451685025;
+  };
 
   services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
