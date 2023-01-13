fe8b50f524a1162ffaa02fa369683d8b318effe8
LegitMagic <76862862+LegitMagic@users.noreply.github.com>
2023-01-13 10:53:38 -0500

    Compat for old nix operations

--- config/default.nix
+++ config/default.nix
@@ -1,10 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, inputs, ... }:
 
 {
   # Nix
   nix = {
     settings.experimental-features = "nix-command flakes";
     settings.auto-optimise-store = true;
+    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
     gc.automatic = true;
     gc.options = "--delete-older-than 30d";
   };
