35cbc7d9023bb2f2dfe81f6a1200afd67e88eb0b
LegitMagic <76862862+LegitMagic@users.noreply.github.com>
2023-01-13 00:36:50 -0500

    Add BROWN NixOS host to flake

---
+++ hosts/BROWN/hardware-configuration.nix
@@ -0,0 +1,63 @@
+{ modulesPath, pkgs, lib, ... }:
+{
+  # Boot
+  imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+  boot = {
+    # Kernel
+    initrd.kernelModules = [ "nvme" ];
+    kernelParams = [ "mitigations=off" "retbleed=off" "panic=1" "boot.panic_on_fail" ];
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    # Boot
+    loader.grub = {
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+      device = "nodev";
+    };
+  };
+  systemd.enableEmergencyMode = false;
+
+  # Hardware
+  nixpkgs.hostPlatform = lib.mkDefault "aarch64-linux";
+  services.fstrim.enable = true;
+
+  # Networking
+  time.timeZone = "America/Louisville";
+  networking = {
+    hostName = "BROWN";
+    nameservers = [ "8.8.8.8" "1.1.1.1" ];
+    useDHCP = false;
+    interfaces.enp0s3.useDHCP = true;
+    firewall.enable = true;
+  };
+
+  # Performance
+  powerManagement.cpuFreqGovernor = "performance";
+
+  # State
+  system.stateVersion = "21.11";
+
+  # Disks
+  boot.cleanTmpDir = true;
+  swapDevices = [ { device = "/swapfile"; size = 16384; } ];
+  fileSystems = {
+    "/" = {
+      device = "/dev/sda1";
+      fsType = "ext4";
+    };
+    "/boot" = {
+      device = "/dev/disk/by-label/UEFI";
+      fsType = "vfat";
+    };
+    "/mnt/Block" = {
+      device = "/dev/disk/by-label/Block";
+      fsType = "ext4";
+    };
+  };
+  
+  # Persistance
+  users.mutableUsers = false;
+
+  # Sops Key File Location
+  sops.age.keyFile = "/root/sops-key.txt";
+}
