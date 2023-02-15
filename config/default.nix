ab2d30fcc6c69b909722bc0e35ac1c40e8c36168
LegitMagic <76862862+LegitMagic@users.noreply.github.com>
2023-02-15 10:56:32 -0500

    Temp workaround for nixpkgs not in $NIX_PATH

--- config/default.nix
+++ config/default.nix
@@ -6,7 +6,7 @@
     settings.experimental-features = "nix-command flakes";
     settings.auto-optimise-store = true;
     registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
-    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+    nixPath = (lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry) ++ [ "nixpkgs=${inputs.nixpkgs-unstable}" ];
     gc.automatic = true;
     gc.options = "--delete-older-than 30d";
   };
