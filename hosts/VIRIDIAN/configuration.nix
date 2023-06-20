4c860854070dcf54b10f2415e4df056078d89433
LegitMagic <76862862+LegitMagic@users.noreply.github.com>
2023-06-20 14:33:19 -0400

    Add navidrome to VIRIDIAN

--- hosts/VIRIDIAN/configuration.nix
+++ hosts/VIRIDIAN/configuration.nix
@@ -7,5 +7,12 @@
       extraConfig = "--disable-login 1";
       interfaces = [ "eth0" ];
     };
+    navidrome = {
+      enable = true;
+      settings = {
+        Address = "0.0.0.0";
+        MusicFolder = "/mnt/Seagate3TB/Music";
+      };
+    };
   };
 }
