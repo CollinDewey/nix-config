7b9b3ecee591507e490e7247fb863b0dc62e4515
Collin Dewey <76862862+LegitMagic@users.noreply.github.com>
2023-09-08 01:18:35 +0000

    VIRIDIAN: Remove ntopng

--- hosts/VIRIDIAN/configuration.nix
+++ hosts/VIRIDIAN/configuration.nix
@@ -2,14 +2,6 @@
 {
   services = {
     netdata.enable = true;
-    ntopng = {
-      enable = true;
-      interfaces = [ "eth0" ];
-      extraConfig = ''
-        --disable-login 1
-        --local-networks 172.16.0.0/24=Main,172.16.1.0/24=IoT
-      '';
-    };
     navidrome = {
       enable = true;
       settings = {
