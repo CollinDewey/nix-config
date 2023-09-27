00390d51eaf461b64dbb75418eb00cfc56ebe767
LegitMagic <76862862+LegitMagic@users.noreply.github.com>
2023-09-27 03:15:57 -0400

    TEAL: mdadm

--- hosts/TEAL/external-disks.nix
+++ hosts/TEAL/external-disks.nix
@@ -4,6 +4,11 @@ let
 in
 {
   config = lib.mkIf (config.specialisation != {}) {
+    boot.swraid.mdadmConf = ''
+      ARRAY /dev/md0 level=raid0 num-devices=2 metadata=1.2 name=TEAL:0 UUID=e64ff7f5:24973621:e2702cab:83fe4dfb
+         devices=/dev/pci-0000:08:00.0-nvme-1,/dev/pci-0000:0a:00.0-nvme-1
+    '';
+
     fileSystems = {
       "/var/log/syncthing" = {
         device = "/dev/bcache0";
