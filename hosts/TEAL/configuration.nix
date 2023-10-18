f6f38dc0e270a7ae2c58af265b845afcccb26fd4
LegitMagic <76862862+LegitMagic@users.noreply.github.com>
2023-10-18 10:51:20 -0400

    TEAL: NFS subnet change

--- hosts/TEAL/configuration.nix
+++ hosts/TEAL/configuration.nix
@@ -11,13 +11,13 @@ in
     nfs.server = {
       enable = true;
       exports = ''
-        / 172.16.1.0/24(${nfs_opts_rw},crossmnt) 10.133.133.2(${nfs_opts_rw},crossmnt)
-        /mnt/Storage 172.16.1.0/24(${nfs_opts_rw}) 10.133.133.2(${nfs_opts_rw})
-        /var/lib/libvirt 172.16.1.0/24(${nfs_opts_rw}) 10.133.133.2(${nfs_opts_rw})
-        /var/lib/libvirt/images_hdd 172.16.1.0/24(${nfs_opts_rw}) 10.133.133.2(${nfs_opts_rw})
-        /snapshots 172.16.1.0/24(${nfs_opts_ro}) 10.133.133.2(${nfs_opts_ro})
-        /services 172.16.1.0/24(${nfs_opts_rw}) 10.133.133.2(${nfs_opts_rw})
-        /cyber 172.16.1.0/24(${nfs_opts_rw}) 10.133.133.2(${nfs_opts_rw})
+        / 172.16.1.0/24(${nfs_opts_rw},crossmnt) 10.133.0.0/16(${nfs_opts_rw},crossmnt)
+        /mnt/Storage 172.16.1.0/24(${nfs_opts_rw}) 10.133.0.0/16(${nfs_opts_rw})
+        /var/lib/libvirt 172.16.1.0/24(${nfs_opts_rw}) 10.133.0.0/16(${nfs_opts_rw})
+        /var/lib/libvirt/images_hdd 172.16.1.0/24(${nfs_opts_rw}) 10.133.0.0/16(${nfs_opts_rw})
+        /snapshots 172.16.1.0/24(${nfs_opts_ro}) 10.133.0.0/16(${nfs_opts_ro})
+        /services 172.16.1.0/24(${nfs_opts_rw}) 10.133.0.0/16(${nfs_opts_rw})
+        /cyber 172.16.1.0/24(${nfs_opts_rw}) 10.133.0.0/16(${nfs_opts_rw})
       '';
     };
   };
