4326971c85c556c131c4f5c8f3ce898b70013835
LegitMagic <76862862+LegitMagic@users.noreply.github.com>
2023-10-30 01:09:55 -0400

    Update flake lock

--- config/home.nix
+++ config/home.nix
@@ -2,4 +2,7 @@
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.permittedInsecurePackages = [ # NixOS/nixpkgs#263764
+    "electron-24.8.6"
+  ];
 }
