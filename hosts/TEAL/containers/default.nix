88a661274a3a72c0e6370743c9879d051ee95697
LegitMagic <76862862+LegitMagic@users.noreply.github.com>
2024-01-02 18:39:28 -0500

    TEAL: Container Networking + LANCache Container

--- hosts/TEAL/containers/default.nix
+++ hosts/TEAL/containers/default.nix
@@ -1,9 +1,11 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  # Currently connecting a bridge (virbr2) to my OPNSense VM, created through virsh
+
   networking.nat = {
     enable = true;
-    internalInterfaces = ["ve-adguardhome"];
-    externalInterface = "enp5s0f0";
+    #internalInterfaces = [""];
+    #externalInterface = "enp5s0f0";
   };
 
   containers = {
@@ -41,6 +43,7 @@
     adguardhome = {
       ephemeral = true;
       autoStart = true;
+      macvlans = [ "virbr2" ];
       config = ./adguardhome.nix;
       bindMounts = {
         "/var/lib/private/AdGuardHome" = {
@@ -62,6 +65,31 @@
           hostPath = "/services/microbin";
           isReadOnly = false;
         };
+      };
+    };
+  };
+
+  system.activationScripts.mkContainerNet = ''
+    ${pkgs.docker} network inspect container_net >/dev/null 2>&1 || ${pkgs.docker} network create -d macvlan --subnet=10.111.111.0/24 --gateway=10.111.111.1 -o parent=virbr2 container_net
+  '';
+
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers = {
+      lancache = {
+        image = "lancachenet/monolithic:latest";
+        hostname = "lancache";
+        extraOptions = [ "--network=container_net" "--ip=10.111.111.2"];
+        autoStart = true;
+        environment = {
+          USE_GENERIC_CACHE = "TRUE";
+          CACHE_DISK_SIZE = "500g";
+          TZ = "America/Louisville";
+        };
+        volumes = [
+          "/clearable/lancache/cache:/data/cache"
+          "/clearable/lancache/logs:/data/logs"
+        ];
       };
     };
   };
