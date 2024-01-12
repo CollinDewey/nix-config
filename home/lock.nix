8c53bb6c46e73deab5ebfaa52b807ea2d18e3283
LegitMagic <76862862+LegitMagic@users.noreply.github.com>
2024-01-12 13:54:30 -0500

    Lock: Select systemd lock on Wayland (KDE is not wlroots)

--- home/lock.nix
+++ home/lock.nix
@@ -12,50 +12,54 @@ let
       D='#4f97d7ee' # default
       W='#f22272dd' # wrong
       
-      i3lock-color \
-      \
-      --line-uses-inside \
-      --inside-color=$B \
-      --ring-color=$D \
-      --separator-color=$W \
-      \
-      --keyhl-color=$W \
-      --bshl-color=$W \
-      \
-      --blur 5 \
-      --ignore-empty-password \
-      --clock \
-      --indicator \
-      --noinput-text="" \
-      \
-      --time-color=$D \
-      --time-str="%H %M" \
-      --time-font="Iosevka SS01:bold" \
-      --time-size=40 \
-      --time-pos="ix:iy - 10" \
-      \
-      --date-color=$D \
-      --date-str="%m %d" \
-      --date-font="Iosevka SS01:bold" \
-      --date-size=18 \
-      --date-size=40 \
-      --date-pos="ix:iy + 45" \
-      \
-      --verif-color=$D \
-      --ringver-color=$D \
-      --insidever-color=$C \
-      --verif-text="Verifying" \
-      --verif-font="Iosevka SS01:bold" \
-      --verif-size=32 \
-      --verif-pos="ix:iy + 15" \
-      \
-      --wrong-color=$W \
-      --ringwrong-color=$W \
-      --insidewrong-color=$C \
-      --wrong-text="Incorrect" \
-      --wrong-font="Iosevka SS01:bold" \
-      --wrong-size=32 \
-      --wrong-pos="ix:iy + 15"
+      if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+        loginctl lock-session
+      else
+        i3lock-color \
+        \
+        --line-uses-inside \
+        --inside-color=$B \
+        --ring-color=$D \
+        --separator-color=$W \
+        \
+        --keyhl-color=$W \
+        --bshl-color=$W \
+        \
+        --blur 5 \
+        --ignore-empty-password \
+        --clock \
+        --indicator \
+        --noinput-text="" \
+        \
+        --time-color=$D \
+        --time-str="%H %M" \
+        --time-font="Iosevka SS01:bold" \
+        --time-size=40 \
+        --time-pos="ix:iy - 10" \
+        \
+        --date-color=$D \
+        --date-str="%m %d" \
+        --date-font="Iosevka SS01:bold" \
+        --date-size=18 \
+        --date-size=40 \
+        --date-pos="ix:iy + 45" \
+        \
+        --verif-color=$D \
+        --ringver-color=$D \
+        --insidever-color=$C \
+        --verif-text="Verifying" \
+        --verif-font="Iosevka SS01:bold" \
+        --verif-size=32 \
+        --verif-pos="ix:iy + 15" \
+        \
+        --wrong-color=$W \
+        --ringwrong-color=$W \
+        --insidewrong-color=$C \
+        --wrong-text="Incorrect" \
+        --wrong-font="Iosevka SS01:bold" \
+        --wrong-size=32 \
+        --wrong-pos="ix:iy + 15"
+      fi
     '';
   };
 in
