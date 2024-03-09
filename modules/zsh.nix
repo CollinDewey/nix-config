5b127d7dc0ea8e266d5cff4b6e4e2f228f75fc6f
CollinDewey <collin@dewey.net>
2024-03-09 15:31:24 -0500

    Update ZSH Module

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -5,14 +5,8 @@ let
   cfg = config.modules.zsh;
   agnoster-nix = pkgs.fetchgit {
     url = "https://gist.github.com/CollinDewey/2e6b6c5b257d2f6895603ddb160e6f1d";
-    rev = "ac34ed17fcf5f18d93c6a9a6c9801b9a8699f271";
-    sha256 = "sha256-0gdv/TOvsIpZz2rFzB2V90fJxXLGDQFx+FGwL1WpPTM=";
-  };
-  zsh-defer = pkgs.fetchFromGitHub {
-    owner = "romkatv";
-    repo = "zsh-defer";
-    rev = "57a6650ff262f577278275ddf11139673e01e471";
-    sha256 = "sha256-/rcIS2AbTyGw2HjsLPkHtt50c2CrtAFDnLuV5wsHcLc=";
+    rev = "804f5d1bd9cb37c18cba252ff4eebf62dadc5c7f";
+    sha256 = "sha256-iwFVyG3Np+vLJMhbW8b00mnCXSMY+f69lUGzl8rqfhU=";
   };
 in
 {
@@ -21,10 +15,9 @@ in
     programs.zsh = {
       enable = true;
       interactiveShellInit = ''
-        source "${zsh-defer}/zsh-defer.plugin.zsh"
+        source "${pkgs.zsh-defer}/share/zsh-defer/zsh-defer.plugin.zsh"
         zsh-defer source "${pkgs.zsh-nix-shell}/share/zsh-nix-shell/nix-shell.plugin.zsh";
         zsh-defer source "${pkgs.zsh-fast-syntax-highlighting}/share/zsh/site-functions/fast-syntax-highlighting.plugin.zsh";
-        zsh-defer source "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh";
         zsh-defer source "${pkgs.oh-my-zsh}/share/oh-my-zsh/plugins/sudo/sudo.plugin.zsh";
         zsh-defer source "${pkgs.zsh-history-substring-search}/share/zsh-history-substring-search/zsh-history-substring-search.zsh"
         bindkey "$terminfo[kcuu1]" history-substring-search-up
