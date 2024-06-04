5baea2843f72ece0d32ed6d770af811dff1bafa5
CollinDewey <collin@dewey.net>
2024-06-04 07:49:42 -0400

    SSH in initrd

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,11 +1,26 @@
 { pkgs, lib, config, ... }:
 
 with lib;
-let cfg = config.modules.ssh;
-
+let
+  cfg = config.modules.ssh;
+  keys = builtins.fetchurl {
+    url = "https://github.com/CollinDewey.keys";
+    sha256 = "sha256:0f6j55wszsxg7kpwlf7p6av2mpkw3djpx35inqy8a97dh8hjyx7q";
+  };
 in {
   options.modules.ssh = { enable = mkEnableOption "ssh"; };
   config = mkIf cfg.enable {
+    # initrd SSH
+    boot.initrd.network = {
+      enable = true;
+      ssh = {
+        enable = true;
+        authorizedKeyFiles = [ keys ];
+        ignoreEmptyHostKeys = true;
+      };
+    };
+
+    # Userland OpenSSH
     services.openssh.enable = true;
     services.openssh.settings = {
       PermitRootLogin = lib.mkForce "no";
