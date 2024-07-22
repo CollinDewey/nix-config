494e058c688947cf154aa2c244e7dad0b9dcb6b1
CollinDewey <collin@dewey.net>
2024-07-22 12:14:06 -0400

    TEAL: Remove zsh_history persist (for now)

--- hosts/TEAL/hardware-configuration.nix
+++ hosts/TEAL/hardware-configuration.nix
@@ -98,7 +98,7 @@
     "/persist" = {
       hideMounts = true;
       files = [
-        { file = "/home/collin/.zsh_history"; parentDirectory = { user = "collin"; group = "collin"; }; } # Full tmpfs home
+        #{ file = "/home/collin/.zsh_history"; parentDirectory = { user = "collin"; group = "collin"; }; } # Full tmpfs home
         { file = "/home/collin/.config/htop/htoprc"; parentDirectory = { user = "collin"; group = "collin"; }; } # Full tmpfs home
         "/etc/machine-id" # Honestly no idea why we need this to be the same between boots
         "/etc/ssh/ssh_host_ed25519_key" # Not reset my host keys
