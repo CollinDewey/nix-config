8c091a7a6f75bd14b6a30a3d38a002b2173437a1
CollinDewey <collin@dewey.net>
2024-09-10 23:29:42 -0400

    BURGUNDY: Auto start noisetorch

--- hosts/BURGUNDY/configuration.nix
+++ hosts/BURGUNDY/configuration.nix
@@ -34,6 +34,21 @@
 
   # Noisetorch
   programs.noisetorch.enable = true;
+  systemd.user.services.noisetorch = {
+    enable = true;
+    requires = [ "sys-devices-pci0000:00-0000:00:1f.3-skl_hda_dsp_generic-sound-card0-controlC0.device" ];
+    after = [ "pipewire.service" "sys-devices-pci0000:00-0000:00:1f.3-skl_hda_dsp_generic-sound-card0-controlC0.device" ];
+    wantedBy = [ "pipewire.service" ];
+    description = "Noisetorch";
+    serviceConfig = {
+        Type = "simple";
+        RemainAfterExit = "yes";
+        ExecStart = "${pkgs.noisetorch}/bin/noisetorch -i -s alsa_input.pci-0000_00_1f.3-platform-skl_hda_dsp_generic.HiFi__Mic1__source -t 95";
+        ExecStop = "${pkgs.noisetorch}/bin/noisetorch -u";
+        Restart = "on-failure";
+        RestartSec = "3";
+    };
+  };
 
   # State
   system.stateVersion = "24.11";
