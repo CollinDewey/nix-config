8512213d515c02de650c67fce66ef40890e9ec63
CollinDewey <collin@dewey.net>
2024-10-25 02:23:50 -0400

    Only enable CUDA if NVIDIA loaded

--- config/home.nix
+++ config/home.nix
@@ -1,7 +1,8 @@
+{ lib, config, ... }:
 {
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
   home-manager.backupFileExtension = "hm-backup";
   nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.cudaSupport = true;
+  nixpkgs.config.cudaSupport = lib.elem "nvidia" config.services.xserver.videoDrivers;
 }
