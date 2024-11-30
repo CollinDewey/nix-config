99e6e68b9d3692b7ee309803c3d440a64e9240d0
CollinDewey <collin@dewey.net>
2024-11-29 23:45:42 -0500

    Temporary orca-slicer workaround

--- home/klipper.nix
+++ home/klipper.nix
@@ -8,7 +8,9 @@ in {
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       #cura
-      orca-slicer
+      (orca-slicer.overrideAttrs (old: { # Until PR 360219 is merged.
+        cmakeFlags = lib.remove "-DFLATPAK=1" old.cmakeFlags or [ ];
+      }))
     ];
   };
 }
