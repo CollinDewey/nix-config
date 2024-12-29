c03883b66f8a7067b46718a14b5de550d02be137
CollinDewey <collin@dewey.net>
2024-12-29 06:59:05 -0500

    Remove orca-slicer override

--- home/klipper.nix
+++ home/klipper.nix
@@ -8,10 +8,7 @@ in {
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       #cura
-      (orca-slicer.overrideAttrs (old: {
-        # Until PR 360219 is merged.
-        cmakeFlags = lib.remove "-DFLATPAK=1" old.cmakeFlags or [ ];
-      }))
+      orca-slicer
     ];
   };
 }
