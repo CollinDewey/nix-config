ead039db9d940a388842b14eed5304e4b31878b8
CollinDewey <collin@dewey.net>
2025-02-20 01:04:51 -0500

    CYBERL Nix Config

--- hosts/CYBERL/system.nix
+++ hosts/CYBERL/system.nix
@@ -17,4 +17,14 @@ in
     package = driver;
     package32 = driver.lib32;
   };
+
+  environment.etc."nix/nix.custom.conf".text = ''
+    experimental-features = nix-command flakes auto-allocate-uids configurable-impure-env
+    auto-allocate-uids = true
+    require-sigs = false
+    build-users-group = nixbld
+    trusted-users = root @wheel
+    substituters = https://nix-community.cachix.org https://cuda-maintainers.cachix.org https://nix-community.cachix.org/ https://chaotic-nyx.cachix.org/ https://cache.nixos.org/
+    trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8=
+  '';
 }
