8e7d0b689b74fd110d8068cb9105f0d9c76c8a29
CollinDewey <collin@dewey.net>
2026-04-04 03:40:16 -0400

    Fix OVMF logo

--- overlays/ovmf-logo.nix
+++ overlays/ovmf-logo.nix
@@ -2,11 +2,20 @@
 {
   nixpkgs.overlays = [
     (final: prev: {
-      OVMF = prev.OVMF.overrideAttrs (old: {
+      OVMFFull = prev.OVMFFull.overrideAttrs (old: {
         postPatch =
           (old.postPatch or "")
           + ''
             cp ${./Logo.bmp} ./MdeModulePkg/Logo/Logo.bmp
+          '';
+      });
+
+      qemu = prev.qemu.overrideAttrs (old: {
+        postInstall =
+          (old.postInstall or "")
+          + ''
+            cp -f ${final.OVMFFull.fd}/FV/OVMF_CODE.fd $out/share/qemu/edk2-x86_64-code.fd
+            cp -f ${final.OVMFFull.fd}/FV/OVMF_CODE.ms.fd $out/share/qemu/edk2-x86_64-secure-code.fd
           '';
       });
     })
