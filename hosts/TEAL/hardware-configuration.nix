95a4ea11a872f77ad7e36429b775b8edea0d20ad
CollinDewey <collin@dewey.net>
2026-09-02 13:53:19 -0400

    TEAL: Fix boot due to no TPM2 breaking systemd-creds

--- hosts/TEAL/hardware-configuration.nix
+++ hosts/TEAL/hardware-configuration.nix
@@ -206,6 +206,7 @@
         "/etc/ssh/ssh_host_rsa_key.pub" # Not reset my host keys
       ];
       directories = [
+        { directory = "/var/lib/systemd"; user = "root"; group = "root"; }
         { directory = "/var/lib/copyparty"; user = "collin"; group = "collin"; }
         { directory = "/home/collin/.config/Moonlight Game Streaming Project"; user = "collin"; group = "collin"; }
       ];
