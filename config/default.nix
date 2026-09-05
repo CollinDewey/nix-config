db14d27d185186e6b1b95125c7007a8db2aa9a2b
CollinDewey <collin@dewey.net>
2026-09-05 09:24:41 -0400

    Change experimental features to be a list of strings instead of a singular string

--- config/default.nix
+++ config/default.nix
@@ -9,7 +9,7 @@
   nix = {
     optimise.automatic = true;
     settings = {
-      experimental-features = "nix-command flakes";
+      experimental-features = [ "nix-command" "flakes" ];
       trusted-users = [ "@wheel" ];
       substituters = [ "https://cache.nixos-cuda.org" ];
       trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
